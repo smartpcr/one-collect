30e2da7743100dcbb1fc75f497f88ebb18ca9977
Beau Belgrave <beaub@linux.microsoft.com>
2025-09-17 00:10:57 +0000

    record-trace: Add FFI library
    Tools that would like to incorporate record-trace/one-collect but
    are not written in Rust need a shared object (.so/.dll).
    
    Add FFI sub-directory and define "RecordTrace" C API for non-Rust
    languages to utilize.
    
    The API is very simple and expandable with minimal ties between Rust and
    non-Rust languages. The same command line passed to the record-trace
    tool is passed to the RecordTrace API along with a callback. The
    callback is given the type of message and the message bytes. If non-zero
    is returned from the callback, the recording stops.
    
    Add record-trace/ffi to github CI build/test tasks.
    
    Signed-off-by: Beau Belgrave <beaub@linux.microsoft.com>

---
+++ record-trace/ffi/src/lib.rs
@@ -0,0 +1,154 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+use engine::commandline::RecordArgs;
+use engine::recorder::Recorder;
+use engine::EngineOutput;
+
+const OUTPUT_NORMAL: i32 = 0;
+const OUTPUT_LIVE: i32 = 1;
+const OUTPUT_ERROR: i32 = 2;
+const OUTPUT_PROGRESS: i32 = 3;
+
+type COutputCallback = extern "C" fn(i32, *const u8, usize) -> i32;
+
+#[no_mangle]
+extern "C" fn RecordTrace(
+    args: *const u8,
+    args_len: usize,
+    callback: Option<COutputCallback>) -> i32 {
+    /* Safety checks */
+    if args.is_null() || callback.is_none() {
+        return 1;
+    }
+
+    let args = unsafe { std::slice::from_raw_parts(args, args_len) };
+    let callback = callback.unwrap();
+
+    if let Ok(args) = std::str::from_utf8(args) {
+        let mut output = EngineOutput::default();
+
+        output.with_normal(move |output| {
+            let bytes = output.as_bytes();
+            callback(OUTPUT_NORMAL, bytes.as_ptr(), bytes.len())
+        });
+
+        output.with_live(move |output| {
+            let bytes = output.as_bytes();
+            callback(OUTPUT_LIVE, bytes.as_ptr(), bytes.len())
+        });
+
+        output.with_error(move |output| {
+            let bytes = output.as_bytes();
+            callback(OUTPUT_ERROR, bytes.as_ptr(), bytes.len())
+        });
+
+        output.with_progress(move |output| {
+            let bytes = output.as_bytes();
+            callback(OUTPUT_PROGRESS, bytes.as_ptr(), bytes.len())
+        });
+
+        let parser = RecordArgsParser::new(
+            "RecordTraceFFI",
+            args);
+
+        let mut recorder = Recorder::new(
+            RecordArgs::parse(parser),
+            output);
+
+        recorder.run()
+    } else {
+        1
+    }
+}
+
+pub struct RecordArgsParser<'a> {
+    binary: &'a str,
+    input: &'a str,
+    pos: usize,
+    first: bool,
+}
+
+impl<'a> RecordArgsParser<'a> {
+    pub fn new(binary: &'a str, input: &'a str) -> Self {
+        Self {
+            binary,
+            input,
+            pos: 0,
+            first: true,
+        }
+    }
+}
+
+impl<'a> Iterator for RecordArgsParser<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // First argument is always the "binary"
+        if self.first {
+            self.first = false;
+
+            return Some(self.binary);
+        }
+
+        let bytes = self.input.as_bytes();
+        let len = bytes.len();
+
+        // Skip leading whitespace
+        while self.pos < len && bytes[self.pos].is_ascii_whitespace() {
+            self.pos += 1;
+        }
+
+        if self.pos >= len {
+            return None;
+        }
+
+        let start;
+        let end;
+
+        if bytes[self.pos] == b'"' {
+            // Quoted string
+            self.pos += 1; // skip opening quote
+            start = self.pos;
+
+            while self.pos < len && bytes[self.pos] != b'"' {
+                self.pos += 1;
+            }
+
+            end = self.pos;
+            self.pos += 1; // skip closing quote
+        } else {
+            // Unquoted string
+            start = self.pos;
+            while self.pos < len && !bytes[self.pos].is_ascii_whitespace() {
+                self.pos += 1;
+            }
+            end = self.pos;
+        }
+
+        Some(&self.input[start..end])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn command_parser() {
+        let mut parser = RecordArgsParser::new(
+            "UnitTest",
+            "1 2 3 \"4 5 6\" 7  8  9   \t   10");
+
+        assert_eq!("UnitTest", parser.next().unwrap());
+        assert_eq!("1", parser.next().unwrap());
+        assert_eq!("2", parser.next().unwrap());
+        assert_eq!("3", parser.next().unwrap());
+        assert_eq!("4 5 6", parser.next().unwrap());
+        assert_eq!("7", parser.next().unwrap());
+        assert_eq!("8", parser.next().unwrap());
+        assert_eq!("9", parser.next().unwrap());
+        assert_eq!("10", parser.next().unwrap());
+        assert!(parser.next().is_none());
+    }
+}
